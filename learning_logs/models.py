d5f600eb0312372ed28be69cfa72001680b089d0
MykalB081 <mykalb081@gmail.com>
2021-05-02 13:22:58 -0400

    Update models.py
    - only returns trailing ellipsis if the entry length is more than 50 characters.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -21,4 +21,7 @@ class Entry(models.Model):
         verbose_name_plural = 'entries'
 
     def __str__(self):
-        return f"{self.text[:50]}..."
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        else:
+            return self.text
