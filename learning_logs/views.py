47a6adda53e3577f6ba413311dfb37ab71d05da4
MykalB081 <mykalb081@gmail.com>
2021-05-04 13:57:02 -0400

    User data 2
    - Connecting data to specific user who submitted it
    - To do so, modify Topic model
    - and restricting user submitted data from being viewed by other users
    - associate new topics with current user

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -13,7 +14,7 @@ def index(request):
 @login_required()
 def topics(request):
     """Show all topics."""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
@@ -22,6 +23,8 @@ def topics(request):
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(request.user, topic)
+
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -37,7 +40,9 @@ def new_topic(request):
         # Post data submitted; process data.
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
 
     # Display a blank or invalid form.
@@ -59,6 +64,7 @@ def new_entry(request, topic_id):
         if form.is_valid():
             new_entry = form.save(commit=False)
             new_entry.topic = topic
+            check_topic_owner(request.user, topic)
             new_entry.save()
             return redirect('learning_logs:topic', topic_id=topic_id)
 
@@ -72,6 +78,7 @@ def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    check_topic_owner(request.user, topic)
 
     if request.method != 'POST':
         # Initial request; pre-fill form with the current entry.
@@ -85,3 +92,8 @@ def edit_entry(request, entry_id):
 
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'learning_logs/edit_entry.html', context)
+
+
+def check_topic_owner(request_user, topic):
+    if topic.owner != request_user:
+        raise Http404
